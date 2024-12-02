5bcf51dcc718537dfc8363963fe6acc932faec41
main <zao3662@naver.comgit config --global user.email zao3662@naver.com>
2024-12-02 20:31:04 +0900

    강아지 카테고리 위젯화후 적용

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'dog_information_page.dart';
+import 'dogcategorywidget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -10,7 +12,6 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Container(
-
         height: 700,
         decoration: BoxDecoration(
           color: Colors.grey[200],
@@ -23,7 +24,6 @@ class HomePage extends StatelessWidget {
         margin: EdgeInsets.all(10),
         padding: EdgeInsets.all(10),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 10),
@@ -42,152 +42,89 @@ class HomePage extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 3,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
+                DogCategoryWidget(
+                  imagePath: 'asset/dog_image.png',
+                  title: '소형 강아지',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DogInformationPage(category: '소형'),
                       ),
-                    ],
-                  ),
-                  padding: EdgeInsets.all(10), // 내부 여백
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  DogInformationPage(category: '소형'),
-                            ),
-                          );
-                        },
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: Image.asset(
-                            'asset/dog_image.png',
-                            width: 70,
-                            height: 70,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        '소형 강아지',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 3,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
+                DogCategoryWidget(
+                  imagePath: 'asset/dog_image2.png',
+                  title: '중형 강아지',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DogInformationPage(category: '중형'),
                       ),
-                    ],
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  DogInformationPage(category: '중형'),
-                            ),
-                          );
-                        },
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: Image.asset(
-                            'asset/dog_image2.png',
-                            width: 70,
-                            height: 70,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        '중형 강아지',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 3,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
+                DogCategoryWidget(
+                  imagePath: 'asset/big_dog.png',
+                  title: '대형 강아지',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DogInformationPage(category: '대형'),
                       ),
-                    ],
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  DogInformationPage(category: '대형'),
-                            ),
-                          );
-                        },
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: Image.asset(
-                            'asset/big_dog.png',
-                            width: 70,
-                            height: 70,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DogCategoryWidget(
+                  imagePath: 'asset/longdog.png',
+                  title: '장모종',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DogInformationPage(category: '장모종'),
                       ),
-                      SizedBox(height: 10),
-                      Text(
-                        '대형 강아지',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
+                    );
+                  },
+                ),
+                DogCategoryWidget(
+                  imagePath: 'asset/shortdog.png',
+                  title: '단모종',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DogInformationPage(category: '단모종'),
                       ),
-                    ],
-                  ),
+                    );
+                  },
+                ),
+                DogCategoryWidget(
+                  imagePath: 'asset/dog_rank.png',
+                  title: 'IQ 순위',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DogInformationPage(category: 'IQ 순위'),
+                      ),
+                    );
+                  },
                 ),
               ],
             ),
