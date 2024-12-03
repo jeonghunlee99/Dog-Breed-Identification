484f94fc48b5a98bce05d5a83ab47083c9c7a190
main <zao3662@naver.comgit config --global user.email zao3662@naver.com>
2024-12-03 19:36:55 +0900

    dog class 털길이 , 지능 필드 추가

--- lib/dog.dart
+++ lib/dog.dart
@@ -8,7 +8,8 @@ class Dog {
   final String size;
   final String temperament;
   final String lifeSpan;
-
+  final String coat;
+  final int iqRank;
   Dog({
     required this.name,
     required this.imageUrl,
@@ -16,19 +17,25 @@ class Dog {
     required this.size,
     required this.temperament,
     required this.lifeSpan,
+    required this.coat,
+    required this.iqRank
   });
 
   factory Dog.fromJson(Map<String, dynamic> json) {
     return Dog(
-      name: json['name'],
-      imageUrl: json['image_url'],
-      description: json['description'],
-      size: json['characteristics']['size'],
-      temperament: json['characteristics']['temperament'],
-      lifeSpan: json['characteristics']['life_span'],
+      name: json['name'] ?? '이름 없음',
+      imageUrl: json['image_url'] ?? '',
+      description: json['description'] ?? '설명이 없습니다.',
+      size: json['characteristics']['size'] ?? '크기 정보 없음',
+      temperament: json['characteristics']['temperament'] ?? '성격 정보 없음',
+      lifeSpan: json['characteristics']['life_span'] ?? '수명 정보 없음',
+      coat: json['characteristics']['Coat'] ?? '코트 정보 없음',
+      iqRank: json['characteristics']['iqRank'] ?? 'iq 정보 없음'
     );
   }
 }
+
+
 
 Future<List<Dog>> loadDogs() async {
   try {
