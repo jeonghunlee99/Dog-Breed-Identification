6ed0b1e288ba2c4249cc655e399cf6cac3d43a16
main <zao3662@naver.comgit config --global user.email zao3662@naver.com>
2024-12-06 20:12:03 +0900

    주석 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'; // Riverpod import 추가
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'homepage.dart';
 
 void main() async {
