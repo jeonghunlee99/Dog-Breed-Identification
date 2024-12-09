073097ed987977195a20cb0cde350f73a1c961a7
main <zao3662@naver.comgit config --global user.email zao3662@naver.com>
2024-12-09 20:11:18 +0900

    강아지 산책 관련 기획

--- lib/dog_walk_screen.dart
+++ lib/dog_walk_screen.dart
@@ -16,7 +16,7 @@ class _DogWalkPageState extends State<DogWalkPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('강아지 산책')),
-      body: Center(),
+      body: Text("1.강아지 산책한 날짜 달력에 기록 \n2.산책 통계 페이지 \n3.할수있다면 타이머 추가하여 버튼눌러서 산책시간 저장하여 기록되는 형식 "),
       bottomNavigationBar: CustomBottomNavBar(
         currentIndex: _currentIndex,
         onTap: (index) {
