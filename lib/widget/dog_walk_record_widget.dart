4e611515a6be901fa1159067aec5615ad986a9e5
main <zao3662@naver.comgit config --global user.email zao3662@naver.com>
2024-12-10 20:58:22 +0900

    강아지 산책 달력부분 ui 수정및 기능추가

--- lib/widget/dog_walk_record_widget.dart
+++ lib/widget/dog_walk_record_widget.dart
@@ -5,12 +5,14 @@ class BuildRecordWidget extends StatelessWidget {
   final DateTime selectedDay;
   final Map<DateTime, List<String>> walkEvents;
   final Function(DateTime, DateTime) onDaySelected;
+  final Function(DateTime) onAddWalkRecord;
 
   const BuildRecordWidget({
     super.key,
     required this.selectedDay,
     required this.walkEvents,
     required this.onDaySelected,
+    required this.onAddWalkRecord,
   });
 
   @override
@@ -27,11 +29,83 @@ class BuildRecordWidget extends StatelessWidget {
             final normalizedDate = DateTime(day.year, day.month, day.day);
             return walkEvents[normalizedDate] ?? [];
           },
+          headerStyle: const HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+            titleTextStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF5BDFE5),
+            ),
+          ),
+          calendarStyle: CalendarStyle(
+            selectedDecoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1,
+              ),
+              shape: BoxShape.circle,
+            ),
+            selectedTextStyle: TextStyle(
+              color: Colors.black,
+            ),
+            todayDecoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey,
+                width: 1,
+              ),
+              shape: BoxShape.circle,
+            ),
+            todayTextStyle: TextStyle(
+              color: Colors.grey,
+            ),
+            markerDecoration: BoxDecoration(
+              color: Color(0xFF5BDFE5),
+              shape: BoxShape.circle,
+            ),
+          ),
+          enabledDayPredicate: (day) {
+            return day.isBefore(DateTime.now().add(Duration(days: 0)));
+          },  // 오늘 이후에는 날짜 선택 비활성화 시키기
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: InkWell(
+            onTap: () {
+              onAddWalkRecord(selectedDay);
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.asset(
+                'asset/dog_walk_add.png',
+                width: 150,
+                height: 60,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
         ),
         Expanded(
           child: ListView(
             children: (walkEvents[selectedDay] ?? [])
-                .map((event) => ListTile(title: Text(event)))
+                .map((event) => Card(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      elevation: 3,
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.all(16),
+                        title: Text(
+                          event,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ))
                 .toList(),
           ),
         ),
