d074263f8bf0be77cbc0c893c8aa6cfd7fae7476
main <zao3662@naver.comgit config --global user.email zao3662@naver.com>
2024-12-12 18:37:53 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screen/homepage.dart';
+import 'dart:developer';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: 'asset/config/.env');
+  await _initialize();
   runApp(MyApp());
 }
+
+Future<void> _initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(
+
+      clientId: dotenv.env['clientId']!,     // 클라이언트 ID 설정
+      onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed")
+  );
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
