8610e1063fde6572f94f5fa89b508e096233eca3
main <zao3662@naver.comgit config --global user.email zao3662@naver.com>
2024-12-23 19:29:44 +0900

    commit

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -4,7 +4,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 import 'homepage.dart';
 
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -24,13 +23,15 @@ class _LoginScreenState extends State<LoginScreen> {
         return;
       }
 
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+      await googleUser.authentication;
       final AuthCredential credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
 
-      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+      final UserCredential userCredential =
+      await _auth.signInWithCredential(credential);
       final User? user = userCredential.user;
 
       if (user != null) {
@@ -48,46 +49,79 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        forceMaterialTransparency: true,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(
-              '간편로그인으로 더 다양한,\n서비스를 이용하세요!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: MediaQuery.of(context).size.width * 0.05,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('asset/dog.gif'),
+            fit: BoxFit.cover,
           ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: _signInWithGoogle,
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-                side: const BorderSide(color: Colors.black),
-              ),
-              padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 50),
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const SizedBox(width: 10),
-                const Text(
-                  'Google 로그인',
-                  style: TextStyle(fontSize: 16, color: Colors.black),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 40,
+              left: 16,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                  size: 30,
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.only(bottom: 30),
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 10,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      '간편로그인으로 더 다양한,\n서비스를 이용하세요!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.05,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: _signInWithGoogle,
+                      child: Image.asset('asset/google_sign.png',
+                          width: 200,
+                          height: 45,
+                          fit: BoxFit.cover),
+                    ),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: _signInWithGoogle, // kakao 로그인 미구현
+                      child: Image.asset('asset/kakao_login.png',
+                          width: 200,
+                          height: 45,
+                          fit: BoxFit.cover),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
