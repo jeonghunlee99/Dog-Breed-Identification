2c22626056c8c45e47357446f19005f9526612b6
main <zao3662@naver.comgit config --global user.email zao3662@naver.com>
2024-12-30 17:34:40 +0900

    커스텀 토스트메시지

---
+++ lib/widget/custom_snackbar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomSnackBar {
+  static void show(
+      BuildContext context, {
+        required String message,
+        required Color backgroundColor,
+        required IconData icon,
+      }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Row(
+          children: [
+            Icon(
+              icon,
+              color: Colors.white,
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              child: Text(
+                message,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: backgroundColor,
+        behavior: SnackBarBehavior.floating,
+        margin: const EdgeInsets.only(bottom: 50, left: 20, right: 20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        duration: const Duration(seconds: 3),
+      ),
+    );
+  }
+}
