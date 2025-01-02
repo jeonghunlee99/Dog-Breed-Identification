0e3e89ee0c82659580bbc799b39e4704cf1c8dc2
main <zao3662@naver.comgit config --global user.email zao3662@naver.com>
2025-01-02 20:26:53 +0900

    riverpod 적용

--- lib/widget/dog_health_record.dart
+++ lib/widget/dog_health_record.dart
@@ -1,92 +1,67 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'custom_snackbar.dart';
 import 'dog_health_record_dialog.dart';
 
-
-class HealthRecordWidget extends StatefulWidget {
-  const HealthRecordWidget({super.key});
-
-  @override
-  State<HealthRecordWidget> createState() => _HealthRecordWidgetState();
-}
-
 enum SortOption { byAdded, byNewest, byOldest }
 
-class _HealthRecordWidgetState extends State<HealthRecordWidget> {
-  DateTime? selectedDate;
-  String dateText = '${DateTime.now().year}-${DateTime.now().month}-${DateTime.now().day}'; // 오늘 날짜로 초기화
-  String? errorText;
-  SortOption _sortOption = SortOption.byAdded;
+// SortOption 상태를 관리하기 위한 Riverpod StateProvider
+final sortOptionProvider = StateProvider<SortOption>((ref) => SortOption.byAdded);
+
+class HealthRecordWidget extends ConsumerWidget {
+  const HealthRecordWidget({Key? key}) : super(key: key);
 
   Stream<List<Map<String, String>>> _getHealthRecords() {
-    final User? user = FirebaseAuth.instance.currentUser;
+    final user = FirebaseAuth.instance.currentUser;
     if (user == null) {
       return Stream.value([]);
     }
 
-    return FirebaseFirestore.instance
-        .collection('dogs')
-        .doc(user.uid)
-        .snapshots()
-        .map((snapshot) {
+    return FirebaseFirestore.instance.collection('dogs').doc(user.uid).snapshots().map((snapshot) {
       if (snapshot.exists) {
-        var data = snapshot.data();
-        var healthRecords = data?['health'] as List<dynamic>? ?? [];
-
+        final data = snapshot.data();
+        final healthRecords = data?['health'] as List<dynamic>? ?? [];
         return healthRecords.map((record) {
           return {
             'date': record['date']?.toString() ?? '',
             'memo': record['memo']?.toString() ?? '',
           };
         }).toList();
-      } else {
-        return [];
       }
+      return [];
     });
   }
 
-  DateTime parseDate(String dateStr) {
+  DateTime _parseDate(String dateStr) {
     try {
-      // 'yyyy-MM-dd' 형식으로 날짜를 변환
       return DateFormat('yyyy-MM-dd').parse(dateStr);
     } catch (e) {
-      // 오류가 발생하면 기본 현재 날짜로 처리
       return DateTime.now();
     }
   }
 
-  List<Map<String, String>> _sortRecords(
-      List<Map<String, String>> records, SortOption sortOption) {
+  List<Map<String, String>> _sortRecords(List<Map<String, String>> records, SortOption sortOption) {
     switch (sortOption) {
       case SortOption.byNewest:
-        records.sort((a, b) {
-          DateTime dateA = parseDate(a['date']!); // 날짜 포맷 처리
-          DateTime dateB = parseDate(b['date']!); // 날짜 포맷 처리
-          return dateB.compareTo(dateA); // 최신 순으로 비교
-        });
+        records.sort((a, b) => _parseDate(b['date']!).compareTo(_parseDate(a['date']!)));
         break;
       case SortOption.byOldest:
-        records.sort((a, b) {
-          DateTime dateA = parseDate(a['date']!); // 날짜 포맷 처리
-          DateTime dateB = parseDate(b['date']!); // 날짜 포맷 처리
-          return dateA.compareTo(dateB); // 오래된 순으로 비교
-        });
+        records.sort((a, b) => _parseDate(a['date']!).compareTo(_parseDate(b['date']!)));
         break;
       case SortOption.byAdded:
-        break; // 추가된 순서는 그대로 두기
+        break; // 그대로 유지
     }
     return records;
   }
 
-
-  Future<void> _deleteHealthRecord(String date, String memo) async {
-    final User? user = FirebaseAuth.instance.currentUser;
+  Future<void> _saveHealthRecord(BuildContext parentContext, String date, String memo) async {
+    final user = FirebaseAuth.instance.currentUser;
     if (user == null) {
       CustomSnackBar.show(
-        context,
+        parentContext,
         message: '로그인이 필요합니다.',
         backgroundColor: Colors.red,
         icon: Icons.error,
@@ -95,72 +70,55 @@ class _HealthRecordWidgetState extends State<HealthRecordWidget> {
     }
 
     try {
-      final userDocRef =
-      FirebaseFirestore.instance.collection('dogs').doc(user.uid);
-
-      await userDocRef.update({
-        'health': FieldValue.arrayRemove([
-          {'date': date, 'memo': memo},
-        ]),
-      });
-      CustomSnackBar.show(
-        context,
-        message: '건강 기록이 삭제되었습니다.',
-        backgroundColor: Colors.red,
-        icon: Icons.check_circle,
-      );
-    } catch (e) {
-      CustomSnackBar.show(
-        context,
-        message: '오류가 발생했습니다: $e',
-        backgroundColor: Colors.red,
-        icon: Icons.error,
-      );
-      print('$e');
-    }
-  }
-
-  Future<void> _saveHealthRecord(String date, String memo) async {
-    final User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      CustomSnackBar.show(
-        context,
-        message: '로그인이 필요합니다.',
-        backgroundColor: Colors.red,
-        icon: Icons.error,
-      );
-      return;
-    }
-
-    try {
-      final userDocRef =
-      FirebaseFirestore.instance.collection('dogs').doc(user.uid);
+      final userDocRef = FirebaseFirestore.instance.collection('dogs').doc(user.uid);
 
       await userDocRef.update({
         'health': FieldValue.arrayUnion([
           {'date': date, 'memo': memo},
         ]),
       });
+
       CustomSnackBar.show(
-        context,
+        parentContext,
         message: '건강 기록이 저장되었습니다.',
         backgroundColor: Colors.green,
         icon: Icons.check_circle,
       );
     } catch (e) {
       CustomSnackBar.show(
-        context,
+        parentContext,
         message: '오류가 발생했습니다: $e',
         backgroundColor: Colors.red,
         icon: Icons.error,
       );
-      print('$e');
     }
   }
 
+  Future<void> _deleteHealthRecord(BuildContext context, String date, String memo) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      CustomSnackBar.show(context, message: '로그인이 필요합니다.', backgroundColor: Colors.red, icon: Icons.error);
+      return;
+    }
+
+    try {
+      final userDocRef = FirebaseFirestore.instance.collection('dogs').doc(user.uid);
+
+      await userDocRef.update({
+        'health': FieldValue.arrayRemove([
+          {'date': date, 'memo': memo},
+        ]),
+      });
+      CustomSnackBar.show(context, message: '건강 기록이 삭제되었습니다.', backgroundColor: Colors.red, icon: Icons.check_circle);
+    } catch (e) {
+      CustomSnackBar.show(context, message: '오류가 발생했습니다: $e', backgroundColor: Colors.red, icon: Icons.error);
+    }
+  }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sortOption = ref.watch(sortOptionProvider);
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: ListView(
@@ -171,21 +129,13 @@ class _HealthRecordWidgetState extends State<HealthRecordWidget> {
           ),
           const SizedBox(height: 10),
           ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all(Colors.white),
-            ),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
             onPressed: () {
-              setState(() {
-                errorText = null;
-              });
-
               showDialog(
                 context: context,
-                builder: (BuildContext context) {
+                builder: (dialogContext) {
                   return HealthRecordDialog(
-                    onSave: (date, memo) {
-                      _saveHealthRecord(date, memo);
-                    },
+                    onSave: (date, memo) => _saveHealthRecord(context, date, memo), // 부모 context 전달
                   );
                 },
               );
@@ -204,30 +154,17 @@ class _HealthRecordWidgetState extends State<HealthRecordWidget> {
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               DropdownButton<SortOption>(
-                value: _sortOption,
-                onChanged: (SortOption? newOption) {
+                value: sortOption,
+                onChanged: (newOption) {
                   if (newOption != null) {
-                    setState(() {
-                      _sortOption = newOption;
-                    });
+                    ref.read(sortOptionProvider.notifier).state = newOption;
                   }
                 },
                 items: const [
-                  DropdownMenuItem(
-                    value: SortOption.byAdded,
-                    child: Text('추가된 순서'),
-                  ),
-                  DropdownMenuItem(
-                    value: SortOption.byNewest,
-                    child: Text('최신 날짜 순'),
-                  ),
-                  DropdownMenuItem(
-                    value: SortOption.byOldest,
-                    child: Text('오래된 날짜 순'),
-                  ),
+                  DropdownMenuItem(value: SortOption.byAdded, child: Text('추가된 순서')),
+                  DropdownMenuItem(value: SortOption.byNewest, child: Text('최신 날짜 순')),
+                  DropdownMenuItem(value: SortOption.byOldest, child: Text('오래된 날짜 순')),
                 ],
-                style: const TextStyle(color: Colors.black),
-                dropdownColor: Colors.white,
               ),
             ],
           ),
@@ -236,7 +173,7 @@ class _HealthRecordWidgetState extends State<HealthRecordWidget> {
             stream: _getHealthRecords(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
+                return const Center(child: CircularProgressIndicator());
               }
 
               if (snapshot.hasError) {
@@ -244,7 +181,7 @@ class _HealthRecordWidgetState extends State<HealthRecordWidget> {
               }
 
               var records = snapshot.data ?? [];
-              records = _sortRecords(records, _sortOption);
+              records = _sortRecords(records, sortOption);
 
               return ListView.builder(
                 shrinkWrap: true,
@@ -273,10 +210,7 @@ class _HealthRecordWidgetState extends State<HealthRecordWidget> {
                         ),
                         IconButton(
                           icon: const Icon(Icons.delete, color: Colors.red),
-                          onPressed: () {
-                            _deleteHealthRecord(
-                                record['date']!, record['memo']!);
-                          },
+                          onPressed: () => _deleteHealthRecord(context, record['date']!, record['memo']!),
                         ),
                       ],
                     ),
